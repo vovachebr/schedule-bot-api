62495db541e452d7e399cfb0ba7767de71b02c4b
Vladimir_Chebukin <Vladimir_Chebukin@epam.com>
2020-03-13 09:22:30 +0300

    update send time

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ const lessons = require('./lessons');
 router.use('/hooks', hooks);
 router.use('/lessons', lessons);
 
-const j = schedule.scheduleJob('01 00 9 * * *', function(){
+const j = schedule.scheduleJob('/30 00 9 * * *', function(){
     const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
 
     mongoClient.connect(function(err, client){
