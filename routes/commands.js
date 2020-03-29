05a6076d54c6769185a5706ca97b587abe48e680
vovachebr <vovachebr@mail.ru>
2020-03-29 16:40:16 +0300

    add moveme slack command

--- routes/commands.js
+++ routes/commands.js
@@ -47,7 +47,47 @@ router.post("/addme", function(request, response){
             });
             client.close();
         });
+    });
+});
 
+router.post("/moveme", function(request, response){
+    const channelName = request.body.text.toLowerCase();
+    const userId = request.body.user_id;
+    const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+    
+    mongoClient.connect(function(err, client){
+        const db = client.db("heroku_4x7x2rvn");
+        const hooksCollection = db.collection("hooks");
+
+        if(err){
+            response.json({ success: false, error: err});
+            client.close();
+            return;
+        } 
+        
+        hooksCollection.findOne({channel: channelName}).then(function(hook){
+            if(!hook){
+                client.close();
+                response.json({
+                    response_type: "ephemeral",
+                    text: "Канал не найден. Обратитесь к координатору курса за помощью."
+                });
+                return;
+            }
+
+            options = {
+                uri: `https://slack.com/api/conversations.invite?token=${SLACK_BOT_TOKEN}&channel=${hook.channelId}&users=${userId}`,
+                method: 'POST'
+            }
+            
+            httpRequest(options, function (error, res, body) {
+                options.uri = options.uri.replace("invite", "kick");
+                options.uri = options.uri.replace("users", "user");
+                options.uri = options.uri.replace(hook.channelId, request.body.channel_id);
+                httpRequest(options);
+            });
+            client.close();
+        });
     });
 });
 
