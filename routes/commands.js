c74ed6ff35c2e06c9a080883eb3aa00827399915
vovachebr <vovachebr@mail.ru>
2020-05-02 19:21:14 +0300

    Refactored slack commands

--- routes/commands.js
+++ routes/commands.js
@@ -1,92 +1,73 @@
-const { MONGODB_URI, SLACK_BOT_TOKEN } = process.env;
+const { SLACK_BOT_TOKEN } = process.env;
 
 const router = require('express').Router();
 const httpRequest = require('request');
-const MongoClient = require("mongodb").MongoClient;
+const { connect } = require('./../util/mongoConnector');
 
-router.post("/addme", function(request, response){
+router.post("/addme", (request, response) => {
     const channelName = request.body.text.toLowerCase();
     const userId = request.body.user_id;
-    const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     
-    mongoClient.connect(function(err, client){
+    connect(async (client) => {
         const db = client.db("schedule");
         const hooksCollection = db.collection("hooks");
-
-        if(err){
-            response.json({ success: false, error: err});
-            client.close();
-            return;
-        } 
         
-        hooksCollection.findOne({channel: channelName}).then(function(hook){
-            if(!hook){
-                client.close();
-                response.json({
-                    response_type: "ephemeral",
-                    text: "Канал не найден. Обратитесь к координатору курса за помощью."
-                });
-                return;
-            }
-
-            options = {
-                uri: `https://slack.com/api/conversations.invite?token=${SLACK_BOT_TOKEN}&channel=${hook.channelId}&users=${userId}`,
-                method: 'POST'
-            }
-            
-            httpRequest(options, function (error, res, body) {
-                response.json({ blocks: [
-                    {
-                        "type": "section",
-                        text:{
-                            "type": "mrkdwn",
-                            text: `Вы добавлены в канал ${channelName}`
-                        }
-                    }
-                ]});
+        const hook = await hooksCollection.findOne({channel: channelName});
+        if(!hook){
+            response.json({
+                response_type: "ephemeral",
+                text: "Канал не найден. Обратитесь к координатору курса за помощью."
             });
-            client.close();
+            return;
+        }
+
+        options = {
+            uri: `https://slack.com/api/conversations.invite?token=${SLACK_BOT_TOKEN}&channel=${hook.channelId}&users=${userId}`,
+            method: 'POST'
+        }
+            
+        httpRequest(options, (error, res, body) => {
+            response.json({ blocks: [
+                {
+                    "type": "section",
+                    text:{
+                        "type": "mrkdwn",
+                        text: `Вы добавлены в канал ${channelName}`
+                    }
+                }
+            ]});
         });
     });
 });
 
-router.post("/moveme", function(request, response){
+router.post("/moveme", (request, response) => {
     const channelName = request.body.text.toLowerCase();
     const userId = request.body.user_id;
-    const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     
-    mongoClient.connect(function(err, client){
+    connect(async (client) => {
         const db = client.db("schedule");
         const hooksCollection = db.collection("hooks");
 
-        if(err){
-            response.json({ success: false, error: err});
+        const hook = await hooksCollection.findOne({channel: channelName});
+        if(!hook){
             client.close();
-            return;
-        } 
-        
-        hooksCollection.findOne({channel: channelName}).then(function(hook){
-            if(!hook){
-                client.close();
-                response.json({
-                    response_type: "ephemeral",
-                    text: "Канал не найден. Обратитесь к координатору курса за помощью."
-                });
-                return;
-            }
-
-            options = {
-                uri: `https://slack.com/api/conversations.invite?token=${SLACK_BOT_TOKEN}&channel=${hook.channelId}&users=${userId}`,
-                method: 'POST'
-            }
-            
-            httpRequest(options, function (error, res, body) {
-                options.uri = options.uri.replace("invite", "kick");
-                options.uri = options.uri.replace("users", "user");
-                options.uri = options.uri.replace(hook.channelId, request.body.channel_id);
-                httpRequest(options);
+            response.json({
+                response_type: "ephemeral",
+                text: "Канал не найден. Обратитесь к координатору курса за помощью."
             });
-            client.close();
+            return;
+        }
+
+        options = {
+            uri: `https://slack.com/api/conversations.invite?token=${SLACK_BOT_TOKEN}&channel=${hook.channelId}&users=${userId}`,
+            method: 'POST'
+        }
+            
+        httpRequest(options, (error, res, body) => {
+            options.uri = options.uri.replace("invite", "kick");
+            options.uri = options.uri.replace("users", "user");
+            options.uri = options.uri.replace(hook.channelId, request.body.channel_id);
+            httpRequest(options);
         });
     });
 });
