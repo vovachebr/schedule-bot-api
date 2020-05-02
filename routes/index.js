0d3db668aced9092a422de641131734878feba93
vovachebr <vovachebr@mail.ru>
2020-05-02 13:37:02 +0300

    Refactoring routes index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const { MONGODB_URI } = process.env;
 const router = require('express').Router();
 const MongoClient = require("mongodb").MongoClient;
 const schedule = require('../schedule');
+const { connect } = require('./../util/mongoConnector');
 
 const hooks = require('./hooks');
 const lessons = require('./lessons');
@@ -19,40 +20,32 @@ router.get("/start", function(request, response) {
     response.send("Запрос отправлен в обработку");
 })
 
-router.post("/sendInstantMessage", function(request, response) {
+router.post("/sendInstantMessage", (request, response) => {
     const {channel, text} = request.body;
     const configer = {
         slack: schedule.sendSlackMessage,
         telegram: schedule.sendTelegramMessage
     };
 
-    const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    mongoClient.connect(function(err, client){
+    connect(async (client) => {
         const db = client.db("schedule");
         const hooksCollection = db.collection("hooks");
-     
-        hooksCollection.find({channel}).toArray(function(errHook, hooks = []){
-            for (let i = 0; i < hooks.length; i++) {
-                const hook = hooks[i];
-                const sender = configer[hook.messegerType];
-                sender(hook, text);
-            }
-            response.json({ success: true});
-            client.close();
-        })
+
+        const hook = await hooksCollection.findOne({channel});
+        const sender = configer[hook.messegerType];
+        sender(hook, text);
+        response.json({ success: true});
     });
 });
 
 router.get("/getDatabaseStat", async function(request, response) {
-    const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    const client = await mongoClient.connect();
- 
-    const stats = await client.db('schedule').stats();
-    const busySize = stats.indexSize + stats.dataSize; //bytes
-    const allSize = 512 * 1024 * 1024; //bytes;
-
-    response.json({ success: true, busySize, allSize});
+    connect(async (client) => {
+        const stats = await client.db('schedule').stats();
+        const busySize = stats.indexSize + stats.dataSize; //bytes
+        const allSize = 512 * 1024 * 1024; //bytes;
+        
+        response.json({ success: true, busySize, allSize});
+    });
 });
 
 module.exports = {router};
