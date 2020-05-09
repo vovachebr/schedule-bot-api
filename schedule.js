e1c7762e386900959a1180ecb0d6d2d1166577a7
vovachebr <vovachebr@mail.ru>
2020-05-09 18:38:21 +0300

    Refactoring scheduler

--- schedule.js
+++ schedule.js
@@ -1,34 +1,23 @@
 const request = require('request');
-const MongoClient = require("mongodb").MongoClient;
 const bot = require('./telegramBot');
 const { getEditImage } = require('./util/imageEditor');
-
-const { MONGODB_URI } = process.env;
+const getLessonText = require('./util/lessonFormatter');
+const { connect } = require('./util/mongoConnector');
 
 function schedule(){
-    const mongoClient = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    mongoClient.connect(function(err, client){
-      
-        if(err) return console.log("err:",err);
-
+    connect(async client => {
         const db = client.db("schedule");
         const lessonsCollection = db.collection("lessons");
         const hooksCollection = db.collection("hooks");
      
         const today = new Date().toISOString().slice(0,10); // сегодня в формате YYYY-MM-DD
-
         
-        lessonsCollection.find({date:today, isSent: false}).toArray(function(errLesson, lessons = []){
-
-            lessons.forEach(lesson => {
-                hooksCollection.findOne({group: lesson.group}, function(errHook, hook){
-                    sendLessonNotification(lesson, hook || {})
-                    
-                })
-            })
-            lessonsCollection.updateMany({date:today}, {$set: {isSent: true}}).then(() => client.close())
+        const lessons = await lessonsCollection.find({date:today, isSent: false}).toArray() || [];
+        lessons.forEach(async lesson => {
+            const hook = await hooksCollection.findOne({group: lesson.group});
+            sendLessonNotification(lesson, hook || {});
         })
+        await lessonsCollection.updateMany({date:today}, {$set: {isSent: true}})
     });
 }
 
@@ -47,7 +36,7 @@ function sendLessonNotification(lesson, hook){
                 },{
                     "type": "image",
                     "image_url": encodeURI(`${process.env.URL}/api/images/getModifiedImage?user=${lesson.teacher}&time=${lesson.time}&date=${lesson.date}&lessonName=${lesson.lecture}`),
-                    "alt_text": "неполучившееся изображение"
+                    "alt_text": "изображение с анонсом занятия"
                 }
             ];
 
@@ -70,36 +59,6 @@ function sendLessonNotification(lesson, hook){
     configuration[hook.messegerType] && configuration[hook.messegerType](lesson, hook); // Вызов конфигурации
 }
 
-function getLessonText(lesson){
-    let template = "Добрый день! \nСегодня, {date}, в {time} по московскому времени состоится лекция «{lecture}». Ее проведет {teacher}. {additional} \n\nСсылку на трансляцию вы найдете в личном кабинете и в письме, которое сегодня придет вам на почту за два часа до лекции.";
-    options = {
-        month: 'numeric',
-        day: 'numeric'
-    };
-    let todayDay = new Date().toISOString().slice(5,10);
-    const getMounth = (num) => [
-        "января",
-        "февраля",
-        "марта",
-        "апреля",
-        "мая",
-        "июня",
-        "июля",
-        "августа",
-        "сентября",
-        "октября",
-        "ноября",
-        "декабря"][num];
-    const splittedData = todayDay.split("-");
-    splittedData[0] = getMounth(+splittedData[0] - 1);
-    splittedData[1] = +splittedData[1];
-    template = template.replace("{date}", splittedData.reverse().join(' '));
-    template = template.replace("{time}", lesson.time);
-    template = template.replace("{teacher}", lesson.teacher);
-    template = template.replace("{lecture}", lesson.lecture);
-    template = template.replace("{additional}", lesson.additional || "");
-    return template;
-}
 
 function sendSlackMessage(hook, data){
     const uri = hook.value;
@@ -122,12 +81,11 @@ function sendSlackMessage(hook, data){
         method: 'POST',
         json: sendData
     }
-    request(options, function (error, response, body) {
-        //TODO: удалить это позже (используется для отладки HTTP запросов в слак)
+    request(options, (error, response, body) => {
         console.error('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
-      });
+    });
 }
 
 function sendTelegramMessage(hook, message){
---
+++ util/lessonFormatter.js
@@ -0,0 +1,32 @@
+function getLessonText(lesson){
+    let template = "Добрый день! \nСегодня, {date}, в {time} по московскому времени состоится лекция «{lecture}». Ее проведет {teacher}. {additional} \n\nСсылку на трансляцию вы найдете в личном кабинете и в письме, которое сегодня придет вам на почту за два часа до лекции.";
+    options = {
+        month: 'numeric',
+        day: 'numeric'
+    };
+    let todayDay = new Date().toISOString().slice(5,10);
+    const getMounth = (num) => [
+        "января",
+        "февраля",
+        "марта",
+        "апреля",
+        "мая",
+        "июня",
+        "июля",
+        "августа",
+        "сентября",
+        "октября",
+        "ноября",
+        "декабря"][num];
+    const splittedData = todayDay.split("-");
+    splittedData[0] = getMounth(+splittedData[0] - 1);
+    splittedData[1] = +splittedData[1];
+    template = template.replace("{date}", splittedData.reverse().join(' '));
+    template = template.replace("{time}", lesson.time);
+    template = template.replace("{teacher}", lesson.teacher);
+    template = template.replace("{lecture}", lesson.lecture);
+    template = template.replace("{additional}", lesson.additional || "");
+    return template;
+}
+
+module.exports = getLessonText;
